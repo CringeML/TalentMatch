96a577648eb8252fd513226c10725854e7182179
Vsevolod Bogodist <vsevolod.bogodist@gmail.com>
2024-02-29 21:17:06 +0300

    хотфикс

--- parser_my.py
+++ parser_my.py
@@ -3,43 +3,6 @@ import json
 import numpy as np
 from algorithm import algorithm
 
-def concat_vacancy(vacancy: dict) -> str:
-    print (vacancy)
-    name = vacancy.get('name', '')
-    keywords = str(vacancy.get('keywords', ''))
-    description = vacancy.get('description', '')
-    comment = vacancy.get('comment', '')
-
-    string = ' '.join(
-        [
-            name if name is not None else '',
-            keywords if keywords is not None else '',
-            description if description is not None else '',
-            comment if comment is not None else '',
-        ]
-    )
-
-    return string
-
-
-def concat_resume(resume: dict):
-    print (str(resume))
-    resume_concated = []
-
-    exp_item = resume.get('experienceItem', '')
-    key_skills = resume.get('key_skills', '')
-
-    if exp_item is not None:
-        for desc in exp_item:
-            description = desc.get('description', '')
-
-            if description is not None:
-                resume_concated.append(description)
-
-    resume_concated = ' '.join(resume_concated) if len(resume_concated) else ''
-
-    return key_skills, resume_concated
-
 
 def parse_json_to_dict(data):
     parsed_data = json.loads(data)
@@ -54,10 +17,7 @@ def eval_json(vacancy_resumes):
     vacancy = vacancy_resumes['vacancy']
     resumes = vacancy_resumes['resumes']
     df_resumes = pd.json_normalize(vacancy_resumes['resumes'])
-    vacancy_str = concat_vacancy(vacancy)
-    resumes_str = [concat_resume(i) for i in resumes]
-
-    df_resumes['relevance'] = algorithm (vacancy_str, resumes_str)
+    df_resumes['relevance'] = algorithm (vacancy, resumes)
 
     return df_resumes
 
